8257ebdfde3c68529dfbded8920532c86b2636bd
Rosie-Rosie19 <chauvetrose01@gmail.com>
2022-11-24 15:20:53 +0100

    Draw a line

--- script.js
+++ script.js
@@ -1,4 +1,29 @@
 console.log('Hello there!');
 
-document.getElementById("circle").style.top = "500px";
+// Je crée une varaible avec let ou var 
+let myCanvas;
 
+// Je définis mon canvas en allant chercher l'élement par son tag.
+// Il s'agit du premier tag donc = [0]
+myCanvas = document.getElementsByTagName('canvas')[0]
+
+// J'ajoute un variable qui signifie "contexte" 
+let ctx = myCanvas.getContext('2d')
+
+//Définition d'un ligne grace aux coordonnée X et Y pour créer un segment
+
+// Affecter une couleur 
+ctx.strokesStyle = "#ffffff"
+
+//Commencer
+ctx.beginPath()
+
+//Tracer
+ctx.moveTo(50, 90)
+ctx.lineTo(150, 100)
+
+//Finir
+ctx.closePath()
+
+//Faire tracer a ligne 
+ctx.stroke()
