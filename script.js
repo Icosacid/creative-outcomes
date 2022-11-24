ef40d9250f421e39e657fc7ca18e94f5275ce1d0
kenzahuber04 <118973215+kenzahuber04@users.noreply.github.com>
2022-11-24 15:48:09 +0100

    2 lines

--- script.js
+++ script.js
@@ -2,20 +2,44 @@ let myCanvas;
 
 myCanvas = document.getElementsByTagName('canvas')[0]
 
+myCanvas.width = window.innerWidth
+myCanvas.height = window.innerHeight
+
 console.log (myCanvas)
 
 let ctx = myCanvas.getContext('2d')
 
 ctx.strokeStyle = "black"
+ctx.fillStyle = "hsl(200, 50%, 60%)"
 
-/* Commencer le chemin*/
+ctx.lineWidth = 1
+
+/* Dessiner une ligne*/
 ctx.beginPath()
 
 ctx.moveTo(200, 50 * Math.random())
-ctx.lineTo(100,100)
+ctx.lineTo(500,500)
 
-
-/* le chemin se termine*/
 ctx.closePath()
-
 ctx.stroke()
+
+ctx.strokeStyle = "#10CC7A"
+
+/* Dessiner une deuxième ligne*/
+ctx.beginPath()
+
+ctx.moveTo(500, 50 * Math.random())
+ctx.lineTo(500,500)
+
+
+ctx.closePath()
+ctx.stroke()
+
+/* Desinner un rectangle*/
+ctx.beginPath()
+ctx.rect(500, 500, 300, 200)
+
+ctx.fill()
+ctx.stroke()
+
+ctx.closePath()
