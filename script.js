3fe4db05bd6f2ca29ecbfc39ae4dae8cfa913aea
Rosie-Rosie19 <chauvetrose01@gmail.com>
2022-11-24 15:47:18 +0100

    Create line and rect

--- script.js
+++ script.js
@@ -7,23 +7,50 @@ let myCanvas;
 // Il s'agit du premier tag donc = [0]
 myCanvas = document.getElementsByTagName('canvas')[0]
 
+//Définition de la taille du canvas
+myCanvas.width = window.innerWidth
+myCanvas.height = window.innerHeight
+
 // J'ajoute un variable qui signifie "contexte" 
 let ctx = myCanvas.getContext('2d')
 
-//Définition d'un ligne grace aux coordonnée X et Y pour créer un segment
 
-// Affecter une couleur 
-ctx.strokesStyle = "#ffffff"
 
+//Définition d'une ligne grace aux coordonnée X et Y pour créer un segment
+ctx.strokeStyle = "#ffffff"// Affecter une couleur de la ligne
+ctx.lineWidth = 2 //Epaisseur de la ligne 
+ctx.fillStyle = "#ffffff" //Couleur du remplissage 
 //Commencer
-ctx.beginPath()
-
-//Tracer
-ctx.moveTo(50, 90)
-ctx.lineTo(150, 100)
-
+ctx.beginPath()//On appelle la méthode, la fonction avec les ()
+//Tracer x to y
+ctx.moveTo(650, 590)
+ctx.lineTo(450, 400)
 //Finir
 ctx.closePath()
-
-//Faire tracer a ligne 
+//Faire tracer la ligne 
 ctx.stroke()
+
+
+//Rectangle
+ctx.beginPath()
+ctx.rect(220, 250, 220, 250)
+ctx.closePath()
+ctx.stroke()
+ctx.fill()
+
+//Nouveau trait et nouveau rectangle 
+ctx.strokeStyle = "pink"
+ctx.lineWidth = 15
+ctx.fillStyle = "pink"
+ctx.fillStyle = "pink"
+ctx.beginPath()
+ctx.moveTo(200, 300)
+ctx.lineTo(200, 500)
+ctx.closePath()
+ctx.stroke()
+
+ctx.beginPath()
+ctx.rect(600, 400, 400, 100)
+ctx.closePath()
+ctx.stroke()
+ctx.fill()
