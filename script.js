15b1a81372eff0eb73789310732d3a5daad3f6d7
kenzahuber04 <118973215+kenzahuber04@users.noreply.github.com>
2022-11-24 15:20:35 +0100

    Canvas Line

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-console.log('Hello!');
+let myCanvas;
+
+myCanvas = document.getElementsByTagName('canvas')[0]
+
+console.log (myCanvas)
+
+let ctx = myCanvas.getContext('2d')
+
+ctx.strokeStyle = "black"
+
+/* Commencer le chemin*/
+ctx.beginPath()
+
+ctx.moveTo(200, 50 * Math.random())
+ctx.lineTo(100,100)
+
+
+/* le chemin se termine*/
+ctx.closePath()
+
+ctx.stroke()
