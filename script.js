d51d7f16f4e177e942eba7a360b4ba521f9c02c4
AliceGitYnov <118973302+AliceGitYnov@users.noreply.github.com>
2022-11-24 15:20:31 +0100

    create canvas

--- script.js
+++ script.js
@@ -2,3 +2,19 @@ var alice = document.getElementsByClassName("fire")[0];
 alice.style.borderRadius= "10%";
 var marine = document.getElementsByClassName("water") [0];
 marine.style.borderRadius= "80%";
+
+let mycanvas;
+mycanvas = document.getElementsByTagName("canvas")[0];
+let ctx = mycanvas.getContext ("2d")
+
+ctx.strokeStyle = "black";
+// ceci est pour définir comment va bouger notre pinceau
+ctx.beginPath()
+ctx.moveTo(20, 50 * Math.random ());
+ctx.lineTo(100, 60);
+
+ctx.closePath ()
+
+// pour qu'il dessine 
+
+ctx.stroke()
