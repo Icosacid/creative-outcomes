a3959a173c469d65a0b46d22523b813ebdb4c722
MayaArcabella <arcabella.maya@gmail.com>
2022-11-24 14:32:59 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
 console.log('Hello!');
+
+var a = 2, b = 12, result; 
+result = a + b;
+
+console.log(result)
+
+a = 34; 
+b = 1;
+result = a/b;
+console.log(result)
+
+aleatoir = Math.random ();
+console.log(aleatoir)
+
+let i = 0 
+do {
+    console.log("Je compte " + i)
+    i = i + 1 
+} while (i < 3)
+
+for(let i=0 ; i<6 ; i++){
+    document.write(i);
+}
