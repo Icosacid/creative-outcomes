a206816d5e482b756dab5a5d3b0757d19291ec4b
laurabastiani <118973196+laurabastiani@users.noreply.github.com>
2022-11-24 15:49:42 +0100

    add elements

--- script.js
+++ script.js
@@ -2,14 +2,46 @@ let myCanvas
 myCanvas = document.
 getElementsByTagName('canvas')[0]
 
+myCanvas.width = window.innerWidth
+myCanvas.height = window.innerHeight
+
 let ctx = myCanvas.getContext('2d')
 
-ctx.strokeStyle = "white"
+ctx.strokeStyle = "#00F51C"
+ctx.fillStyle = "#F500C2"
+ctx.linewidth = 1
+
+// Premier Trait
 ctx.beginPath()
+ctx.moveTo(150, 100)
+ctx.lineTo(500, 150)
+ctx.closePath()
+ctx.stroke()
 
-ctx.moveTo(200, 500)
-ctx.lineTo(100, 60)
+//Deuxieme trait
+ctx.beginPath()
+ctx.moveTo(150, 100)
+ctx.lineTo(500, 100)
+ctx.closePath()
+ctx.stroke()
 
+// Troisième Trait
+ctx.beginPath()
+ctx.moveTo(150, 100)
+ctx.lineTo(500, 50)
+ctx.closePath()
+ctx.stroke()
+
+// Draw a rectangle
+ctx.beginPath()
+ctx.rect(150, 40, 100, 150)
+ctx.fill()
+ctx.stroke()
 ctx.closePath()
 
+// deuxieme rectangle
+ctx.beginPath()
+ctx.rect(500, 40, 100, 150)
+ctx.fill()
 ctx.stroke()
+ctx.closePath()
