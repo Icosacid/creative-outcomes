5ad9ebf7a107be9f51f167ae9bfbb1ea6b48a632
laurabastiani <118973196+laurabastiani@users.noreply.github.com>
2022-11-24 15:20:26 +0100

    create line

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-console.log('Hello!');
+let myCanvas
+myCanvas = document.
+getElementsByTagName('canvas')[0]
+
+let ctx = myCanvas.getContext('2d')
+
+ctx.strokeStyle = "white"
+ctx.beginPath()
+
+ctx.moveTo(200, 500)
+ctx.lineTo(100, 60)
+
+ctx.closePath()
+
+ctx.stroke()
