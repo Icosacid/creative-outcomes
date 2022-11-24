3dcb3e210a2167f4533e00c8c62be64e0e9da98c
AliceGitYnov <118973302+AliceGitYnov@users.noreply.github.com>
2022-11-24 14:28:56 +0100

    add var js

--- script.js
+++ script.js
@@ -1,2 +1,4 @@
 var alice = document.getElementsByClassName("fire")[0];
 alice.style.borderRadius= "10%";
+var marine = document.getElementsByClassName("water") [0];
+marine.style.borderRadius= "80%";
