7ba2e26c74a1a44ded2ef8bdb4998129cac75aaa
AliceGitYnov <118973302+AliceGitYnov@users.noreply.github.com>
2022-11-25 12:58:34 +0100

    commit cercle

--- script.js
+++ script.js
@@ -7,3 +7,12 @@ myCanvas.width = window.innerWidth
 myCanvas.height = window.innerHeight
 
 let ctx = myCanvas.getContext('2d')
+
+//création cercle
+ctx.strokeStyle= "black"
+ctx.fillStyle= "black"
+ctx.beginPath ()
+ctx.arc (1000, 300, 150, 0, 2 * Math.PI, false)
+ctx.fill ()
+ctx.stroke ()
+ctx.closePath ()
