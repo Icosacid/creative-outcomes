ced501045fa482675acdbbbc10b8d95f86e6c2c3
MayaArcabella <arcabella.maya@gmail.com>
2022-11-25 11:56:43 +0100

    Color palette

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ myCanvas.height = window.innerHeight
 
 let ctx = myCanvas.getContext('2d')
 
-/*Création de la palette*/
+/*création de la palette*/
 
 /*couleur de la ligne*/
 ctx.strokeStyle = "#E6DA9F"
@@ -16,9 +16,48 @@ ctx.strokeStyle = "#E6DA9F"
 /*couleur pour remplir forme*/
 ctx.fillStyle = "#E6DA9F"
 
-/* Dessiner un cercle*/
+/*dessiner un cercle*/
 ctx.beginPath()
 ctx.arc(1000, 300, 150, 0, 2 * Math.PI, false)
+ctx.fill()
+ctx.stroke()
+ctx.closePath()
+
+/*couleur de la ligne*/
+ctx.strokeStyle = "#9AC4FA"
+
+/*couleur pour remplir forme*/
+ctx.fillStyle = "#9AC4FA"
+
+/*couleur bleu*/
+ctx.beginPath()
+ctx.arc(1000, 200, 20, 0, 2 * Math.PI, false)
+ctx.fill()
+ctx.stroke()
+ctx.closePath()
+
+/*couleur de la ligne*/
+ctx.strokeStyle = "#FF9778"
+
+/*couleur pour remplir forme*/
+ctx.fillStyle = "#FF9778"
+
+/*color rouge*/
+ctx.beginPath()
+ctx.arc(1000, 400, 20, 0, 2 * Math.PI, false)
+ctx.fill()
+ctx.stroke()
+ctx.closePath()
+
+/*couleur de la ligne*/
+ctx.strokeStyle = "#C1FAC1"
+
+/*couleur pour remplir forme*/
+ctx.fillStyle = "#C1FAC1"
+
+/*color vert*/
+ctx.beginPath()
+ctx.arc(900, 300, 20, 0, 2 * Math.PI, false)
 ctx.fill()
 ctx.stroke()
 ctx.closePath()
