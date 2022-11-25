756688736c37bebdbc31bff7db4cd34b976673c3
kenzahuber04 <118973215+kenzahuber04@users.noreply.github.com>
2022-11-25 10:32:13 +0100

    Created background

--- script.js
+++ script.js
@@ -9,32 +9,50 @@ console.log (myCanvas)
 
 let ctx = myCanvas.getContext('2d')
 
-for (let i = 0; i <20; i++){
-    // Dessiner un cercle 
-    ctx.beginPath()
-    ctx.arc(350, i * 50, 80, 0, 2 * Math.PI, false)  
-    ctx.fillStyle = "#6CB493"
-    ctx.strokeStyle = "white"
-    ctx.lineWidth = 10
-    ctx.closePath()
-    ctx.fill()
-    ctx.stroke()
-    }    
 
-// faire une boucle 
-for (let i = 0; i <20; i++){
-    // Dessiner un cercle 
-    ctx.beginPath()
-    ctx.arc(150, i * 50, 80, 0, 2 * Math.PI, false)  
-    ctx.fillStyle = "#6CB493"
-    ctx.strokeStyle = "white"
-    ctx.lineWidth = 10
-    ctx.closePath()
-    ctx.fill()
-    ctx.stroke()
-    }
-    
-// Faire une boucle   
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* Faire une boucle   
 for (let i = 0; i <20; i++){
     // Desssiner un cercle 
     ctx.beginPath()
@@ -47,24 +65,7 @@ for (let i = 0; i <20; i++){
     ctx.stroke()
     }    
 
-
-
-/* remplir une forme*/
-ctx.strokeStyle = "black"
-ctx.fillStyle = "#6CB493"
-ctx.lineWidth = 1
-
-
-/* Dessiner une ligne*/
-ctx.beginPath()
-ctx.moveTo(400, 30 * Math.random())
-ctx.lineTo(800,500)
-ctx.closePath()
-ctx.stroke()
-ctx.strokeStyle = "#10CC7A"
-
-
-/* Dessiner une deuxième ligne*/
+Dessiner une deuxième ligne
 ctx.beginPath()
 ctx.moveTo(800, 50 * Math.random())
 ctx.lineTo(800,500)
@@ -72,16 +73,7 @@ ctx.closePath()
 ctx.stroke()
 
 
-/* Desinner un rectangle*/
-ctx.beginPath()
-ctx.rect(800, 500, 300, 200)
-ctx.fill()
-ctx.stroke()
-ctx.closePath()
-ctx.fillStyle = "black"
-
-
-/* Desinner un deuxième rectangle*/
+Desinner un deuxième rectangle
 ctx.beginPath()
 ctx.rect(700, 600, 300, 200)
 ctx.fillStyle = 'rgba(255, 0, 255, 0.75)';
@@ -90,9 +82,9 @@ ctx.stroke()
 ctx.closePath()
 
 
-/*for (let i = 0; i < 30; i++) {
+for (let i = 0; i < 30; i++) {
     ctx.lineTo(100 + 5 * i, 120 + 10 * (Math.random() < 0.5) * i)
   }
   
   ctx.stroke()
-  ctx.closePath()*/
+  ctx.closePath() */
