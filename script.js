543e1132273c7943926f18e23387422bdb00ccbb
Rosie-Rosie19 <chauvetrose01@gmail.com>
2022-11-25 15:33:14 +0100

    final_v1

--- script.js
+++ script.js
@@ -11,8 +11,6 @@ let w = myCanvas.width,
     centerY = h / 2
 
 
-<<<<<<< Updated upstream
-=======
 
 // --- variables globales de Marine ---
 var nbDeDisques = 11
@@ -38,7 +36,6 @@ var chosenPalette
 
 
 
->>>>>>> Stashed changes
 //Roselyne 
 
 function curve() {
@@ -66,10 +63,12 @@ function curve() {
 }
 curve()
 
-function lines() {
 
-
-    for (var i = 0; i < 10; i++) {
+var delay = 1000;
+var max = 10;
+var i = 0
+function Loop() {
+    setTimeout(function () {
         ctx.beginPath();
         ctx.strokeStyle = "white"
         ctx.moveTo(i * 20, 20);
@@ -77,37 +76,44 @@ function lines() {
         ctx.lineCap = 'round';
         ctx.lineTo((i * 20) + 2, 20);
         ctx.stroke();
-    }
 
+        ctx.beginPath();
+        ctx.strokeStyle = "white"
+        ctx.moveTo(i * 50, 50);
+        ctx.lineWidth = 15;
+        ctx.lineCap = 'round';
+        ctx.lineTo((i * 50) + 2, 50);
+        ctx.stroke();
 
-    ctx.lineWidth = 9
-    ctx.fillStyle = "black"
-    ctx.strokeStyle = "black"
-    ctx.beginPath();
-    ctx.setLineDash([10, 35]);
-    ctx.arc(1400, 700, 200, 0, 2 * Math.PI, false)
-    ctx.closePath()
-    ctx.stroke()
+        i++;
 
+        if (i < max) {
+            Loop();
+        }
+    }, delay)
+}
+Loop()
 
+function Looping() {
+    setTimeout(function () {
+        ctx.lineWidth = 9
+        ctx.fillStyle = "black"
+        ctx.strokeStyle = "black"
+        ctx.beginPath()
+        ctx.setLineDash(10, 35);
+        ctx.arc(1400, 700, 200, 0, 2 * Math.PI, false)
+        ctx.closePath()
+        ctx.stroke()
+        ctx.fill()
+
+        i++;
+        if (i < max) {
+            Looping();
+        }
+    }, delay)
 
 }
-lines()
-
-// --- variables globales de Marine ---
-var nbDeDisques = 10
-var maxLine = 21
-var items = maxLine * 4
-var r = 70
-    
-var cercleSize = 0.125
-
-var currentItem = 0
-
-var palette = ["#4A90E2", "#4D8ADC", "#4F84D6", "#527DD0", "#5477CA", "#5771C5", "#596BBF", "#5C64B9", "#5E5EB3", "#6158AD", "#6352A7", "#664BA1", "#68459B", "#6B3F95", "#6D398F", "#70328A", "#722C84", "#75267E", "#772078", "#7A1972", "#7C136C"]
-var posPalette = 0
-// -----------------
-
+Looping()
 
 
 
@@ -138,9 +144,6 @@ function createAllCercles() {
     }
     
 }
-
-
-function drawCercle() {
 
 
 
