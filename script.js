89847ad88a2894ceb8d6b87e2cf9ef43c4b6d12d
laurabastiani <118973196+laurabastiani@users.noreply.github.com>
2022-11-25 10:32:14 +0100

    created background

--- script.js
+++ script.js
@@ -11,37 +11,3 @@ ctx.strokeStyle = "#00F51C"
 ctx.fillStyle = "#F500C2"
 ctx.linewidth = 1
 
-// Premier Trait
-ctx.beginPath()
-ctx.moveTo(150, 100)
-ctx.lineTo(500, 150)
-ctx.closePath()
-ctx.stroke()
-
-//Deuxieme trait
-ctx.beginPath()
-ctx.moveTo(150, 100)
-ctx.lineTo(500, 100)
-ctx.closePath()
-ctx.stroke()
-
-// Troisième Trait
-ctx.beginPath()
-ctx.moveTo(150, 100)
-ctx.lineTo(500, 50)
-ctx.closePath()
-ctx.stroke()
-
-// Draw a rectangle
-ctx.beginPath()
-ctx.rect(150, 40, 100, 150)
-ctx.fill()
-ctx.stroke()
-ctx.closePath()
-
-// deuxieme rectangle
-ctx.beginPath()
-ctx.rect(500, 40, 100, 150)
-ctx.fill()
-ctx.stroke()
-ctx.closePath()
