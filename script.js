c9558fff6c6faaf7b0ca2791afaa312ad1f06f2f
AliceGitYnov <118973302+AliceGitYnov@users.noreply.github.com>
2022-11-25 11:01:48 +0100

    Commit création toile

--- script.js
+++ script.js
@@ -7,3 +7,16 @@ myCanvas.width = window.innerWidth
 myCanvas.height = window.innerHeight
 
 let ctx = myCanvas.getContext('2d')
+
+// Création de la toile
+ctx.strokeStyle = "#ffffe0"; //couleur contour toile
+ctx.strokeRect(200, 100, 600, 400);
+
+
+
+
+
+
+
+
+
