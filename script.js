5c1321001a6e77da137390854e511f49e546c610
MayaArcabella <arcabella.maya@gmail.com>
2022-11-25 10:33:10 +0100

    Create Canva

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+/*balise canva*/
 let myCanvas
 
 myCanvas = document.getElementsByTagName('canvas')[0]
@@ -6,55 +7,3 @@ myCanvas.width = window.innerWidth
 myCanvas.height = window.innerHeight
 
 let ctx = myCanvas.getContext('2d')
-//ctx.imageSmoothingEnabled = true
-//ctx.globalCompositeOperation = 'lighter'
-
-/*couleur de la ligne*/
-ctx.strokeStyle = "white"
-
-/*couleur pour remplir forme*/
-ctx.fillStyle = "hsl(200, 50%, 60%)"
-
-/*epaisseur de la ligne*/
-ctx.lineWidth = 3
-
-/*dessiner une ligne*/
-ctx.beginPath()
-ctx.moveTo(20, 50)
-ctx.lineTo(100, 600)
-ctx.stroke()
-ctx.closePath()
-
-/*dessiner un rectangle*/
-ctx.beginPath()
-ctx.rect(10, 50, 20, 30)
-ctx.fill()
-ctx.stroke()
-ctx.closePath()
-
-/*couleur pour remplir forme*/
-ctx.fillStyle = "hsl(100, 50%, 60%)"
-
-/*dessiner une ligne*/
-ctx.beginPath()
-ctx.moveTo(300, 20)
-ctx.lineTo(10, 80)
-ctx.stroke()
-ctx.closePath()
-
-/*dessiner un rectangle*/
-ctx.beginPath()
-ctx.rect(20, 30, 10, 90)
-ctx.fill()
-ctx.stroke()
-ctx.closePath()
-
-/*couleur pour remplir forme*/
-ctx.fillStyle = "hsl(100, 50%, 60%)"
-
-/* dessiner un cercle*/
-ctx.beginPath()
-ctx.arc(200, 300, 100, 0, 2 * Math.PI, false)
-ctx.fill()
-ctx.stroke()
-ctx.closePath()
