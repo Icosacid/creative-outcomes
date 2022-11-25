086e0c27c7646b0595014c9cd4d7e5ba92c3d624
MayaArcabella <arcabella.maya@gmail.com>
2022-11-25 11:11:52 +0100

    add palette

--- script.js
+++ script.js
@@ -7,3 +7,20 @@ myCanvas.width = window.innerWidth
 myCanvas.height = window.innerHeight
 
 let ctx = myCanvas.getContext('2d')
+
+/*Création de la palette*/
+
+/*couleur de la ligne*/
+ctx.strokeStyle = "#E6DA9F"
+
+/*couleur pour remplir forme*/
+ctx.fillStyle = "#E6DA9F"
+
+/* Dessiner un cercle*/
+ctx.beginPath()
+ctx.arc(1000, 300, 150, 0, 2 * Math.PI, false)
+ctx.fill()
+ctx.stroke()
+ctx.closePath()
+
+
