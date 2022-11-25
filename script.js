9504418007155bffba3ed0f98f6aee10290f22bf
kenzahuber04 <118973215+kenzahuber04@users.noreply.github.com>
2022-11-25 16:05:14 +0100

    Finish Work Laura and Kenza

--- script.js
+++ script.js
@@ -1,19 +1,27 @@
-let myCanvas;
-
-myCanvas = document.getElementsByTagName('canvas')[0]
+let myCanvas
+myCanvas = document.
+getElementsByTagName('canvas')[0]
 
 myCanvas.width = 700;
 myCanvas.height = 700;
 
-console.log (myCanvas)
-
 let ctx = myCanvas.getContext('2d')
+
 
 ctx.strokeStyle = "white"
 ctx.fillStyle = "white"
 ctx.linewidth = 1
 
 
+// LAURA
+
+// triangle haut
+ctx.beginPath();
+    ctx.moveTo(350, 350);
+    ctx.lineTo(350, 0);
+    ctx.lineTo(430, 80);
+    ctx.fill();
+    ctx.closePath;
 for (let i = 47; i <100; i++){
     ctx.beginPath()
     ctx.moveTo(0, i*7.5)
@@ -23,6 +31,85 @@ for (let i = 47; i <100; i++){
 }
 
  // Triangle plein
+// triangle bas
+ctx.beginPath();
+    ctx.moveTo(350, 350);
+    ctx.lineTo(350, 700);
+    ctx.lineTo(430, 620);
+    ctx.fill();
+    ctx.closePath;
+
+// triangle du milieu (bas)
+
+ctx.beginPath();
+    ctx.moveTo(350, 350);
+    ctx.lineTo(700, 350);
+    ctx.lineTo(620, 430);
+    ctx.fill();
+    ctx.closePath;
+
+// triangle du milieu (haut)
+    ctx.beginPath();
+    ctx.moveTo(350, 350);
+    ctx.lineTo(700, 350);
+    ctx.lineTo(620, 270);
+    ctx.fill();
+    ctx.closePath;
+
+// trait 1 
+ctx.beginPath();
+ctx.moveTo(430,80);
+ctx.lineTo(620, 270);
+ctx.stroke();
+ctx.closePath;
+// trait perpandiculaire au 1
+ctx.beginPath();
+ctx.moveTo(350,350);
+ctx.lineTo(525, 175);
+ctx.stroke();
+ctx.closePath;
+
+//trait 2
+ctx.beginPath();
+ctx.moveTo(430, 620);
+ctx.lineTo(620, 430);
+ctx.stroke();
+ctx.closePath;
+// trait perdendiculaire au 2
+ctx.beginPath();
+ctx.moveTo(350,350);
+ctx.lineTo(525, 525);
+ctx.stroke();
+ctx.closePath;
+
+//boucle coin en haut a gauche
+for (let i = 47; i < 100; i++) {
+  ctx.beginPath();
+  ctx.moveTo(i*7.5, 0);
+  ctx.lineTo (i*15, 359);
+  ctx.stroke();
+  ctx.closePath();
+}
+//triangle bas
+ctx.beginPath();
+    ctx.moveTo(700, 700);
+    ctx.lineTo(430, 620);
+    ctx.lineTo(620, 430);
+    ctx.fill();
+    ctx.closePath;
+// le trait dedans 
+ctx.beginPath();
+ctx.strokeStyle = "black"
+ctx.moveTo(525,525);
+ctx.lineTo(700, 700);
+ctx.stroke();
+ctx.closePath;
+
+
+// KENZA
+
+ctx.strokeStyle = "white"
+// Triangle plein
  ctx.beginPath();
  ctx.moveTo(350, 350);
  ctx.lineTo(350, 0);
@@ -30,7 +117,7 @@ for (let i = 47; i <100; i++){
  ctx.fill();
  ctx.closePath();
 
- // Triangle plein
+// Triangle plein
  ctx.beginPath();
  ctx.moveTo(350, 350);
  ctx.lineTo(0, 350);
@@ -38,7 +125,7 @@ for (let i = 47; i <100; i++){
  ctx.fill();
  ctx.closePath();
 
- // Triangle plein
+// Triangle plein
  ctx.beginPath();
  ctx.moveTo(350, 350);
  ctx.lineTo(0, 350);
@@ -61,8 +148,6 @@ ctx.lineTo(270, 80);
 ctx.lineTo(80, 270);
 ctx.fill();
 ctx.closePath();
-
-
 
 /*Première ligne du triangle du haut*/
 ctx.beginPath()
