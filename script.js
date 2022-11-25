962cc14c5e1fb32a4c5734956b31b704c92c6452
marinecvlr <56078127+marinecvlr@users.noreply.github.com>
2022-11-25 11:07:45 +0100

    Add first cercle
    ajout d'un premier cercle de ma fonction + suppression de fonctions inutiles

--- script.js
+++ script.js
@@ -1,24 +1,33 @@
 let myCanvas
 myCanvas = document.getElementsByTagName('canvas')[0]
+myCanvas.width = window.innerWidth
+myCanvas.height = window.innerHeight
 let ctx = myCanvas.getContext('2d')
 
+let w = myCanvas.width,
+    h = myCanvas.height,
+    centerX = w / 2,
+    centerY = h / 2
 
 
 
+cercles();
 
+// Cercles Marine
+function cercles() {
 
+    var items = 20;
+    var r = 50;
 
-
-function CartToPol(x, y, r, o) {
-
-    r = Math.sqrt(x*x + y*y)
-    o = Math.atan2(y,x) * 180 / Math.PI;
-}
-
-function PolToCart(x, y, r, o) {
-    var x = r * Math.cos(o)
-    var y = r* Math.sin(o)
-
-    console.log(x)
-    console.log(y)
+    for(var i = 0; i < items; i++) {
+    
+        var x = centerX + r * Math.cos(2 * Math.PI * i / items);
+        var y = centerY + r * Math.sin(2 * Math.PI * i / items);   
+        
+        ctx.beginPath();
+        ctx.lineWidth="2";
+        ctx.fillStyle="#FF4422"
+        ctx.arc(x, y, 5, 0, 2 * Math.PI);
+        ctx.fill();
+    }
 }
