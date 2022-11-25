afb199b175627a1257b091156084467e27958bc8
AliceGitYnov <118973302+AliceGitYnov@users.noreply.github.com>
2022-11-25 13:34:50 +0100

    Commit system solar

--- script.js
+++ script.js
@@ -8,7 +8,88 @@ myCanvas.height = window.innerHeight
 
 let ctx = myCanvas.getContext('2d')
 
-//création cercle
+//création planetes
+ctx.strokeStyle= "black"
+ctx.fillStyle= "black"
+ctx.beginPath ()
+ctx.arc (1000, 300, 150, 0, 2 * Math.PI, false)
+ctx.fill ()
+ctx.stroke ()
+ctx.closePath ()
+
+//Mercure
+ctx.strokeStyle= "black"
+ctx.fillStyle= "black"
+ctx.beginPath ()
+ctx.arc (1000, 300, 150, 0, 2 * Math.PI, false)
+ctx.fill ()
+ctx.stroke ()
+ctx.closePath ()
+
+//Venus
+ctx.strokeStyle= "black"
+ctx.fillStyle= "black"
+ctx.beginPath ()
+ctx.arc (1000, 300, 150, 0, 2 * Math.PI, false)
+ctx.fill ()
+ctx.stroke ()
+ctx.closePath ()
+
+//Terre
+ctx.strokeStyle= "black"
+ctx.fillStyle= "black"
+ctx.beginPath ()
+ctx.arc (1000, 300, 150, 0, 2 * Math.PI, false)
+ctx.fill ()
+ctx.stroke ()
+ctx.closePath ()
+
+//Mars
+ctx.strokeStyle= "black"
+ctx.fillStyle= "black"
+ctx.beginPath ()
+ctx.arc (1000, 300, 150, 0, 2 * Math.PI, false)
+ctx.fill ()
+ctx.stroke ()
+ctx.closePath ()
+
+//Jupiter
+ctx.strokeStyle= "black"
+ctx.fillStyle= "black"
+ctx.beginPath ()
+ctx.arc (1000, 300, 150, 0, 2 * Math.PI, false)
+ctx.fill ()
+ctx.stroke ()
+ctx.closePath ()
+
+//Saturne
+ctx.strokeStyle= "black"
+ctx.fillStyle= "black"
+ctx.beginPath ()
+ctx.arc (1000, 300, 150, 0, 2 * Math.PI, false)
+ctx.fill ()
+ctx.stroke ()
+ctx.closePath ()
+
+//Uranus
+ctx.strokeStyle= "black"
+ctx.fillStyle= "black"
+ctx.beginPath ()
+ctx.arc (1000, 300, 150, 0, 2 * Math.PI, false)
+ctx.fill ()
+ctx.stroke ()
+ctx.closePath ()
+
+//Neptune
+ctx.strokeStyle= "black"
+ctx.fillStyle= "black"
+ctx.beginPath ()
+ctx.arc (1000, 300, 150, 0, 2 * Math.PI, false)
+ctx.fill ()
+ctx.stroke ()
+ctx.closePath ()
+
+//Soleil
 ctx.strokeStyle= "black"
 ctx.fillStyle= "black"
 ctx.beginPath ()
