af2182379d022a2c1a8bf3659c5c50cb723b85c7
marinecvlr <56078127+marinecvlr@users.noreply.github.com>
2022-11-25 14:17:46 +0100

    Merge our codes

--- script.js
+++ script.js
@@ -10,6 +10,25 @@ let w = myCanvas.width,
     centerX = w / 2,
     centerY = h / 2
 
+
+
+// --- variables globales de Marine ---
+var nbDeDisques = 10
+var maxLine = 21
+var items = maxLine * 4
+var r = 70
+    
+var cercleSize = 0.125
+
+var currentItem = 0
+
+var palette = ["#4A90E2", "#4D8ADC", "#4F84D6", "#527DD0", "#5477CA", "#5771C5", "#596BBF", "#5C64B9", "#5E5EB3", "#6158AD", "#6352A7", "#664BA1", "#68459B", "#6B3F95", "#6D398F", "#70328A", "#722C84", "#75267E", "#772078", "#7A1972", "#7C136C"]
+var posPalette = 0
+// -----------------
+
+
+
+
 //Roselyne 
 
 function curve() {
@@ -17,7 +36,6 @@ function curve() {
     ctx.lineWidth = 5
     ctx.fillStyle = "#0D47A1"
 
-<<<<<<< Updated upstream
     ctx.beginPath()
     ctx.moveTo(0, 0)
     ctx.quadraticCurveTo(500, 25, 100, 400);
@@ -36,28 +54,49 @@ function curve() {
     
 }
 curve()
+createAllCercles()
 
-=======
-createAllCercles();
->>>>>>> Stashed changes
+
 
 // Cercles Marine
 function createAllCercles() {
 
-    var items = 84
-    var r = 70
-    var cercleSize = 0.125
-    var currentItem = 0
+    for(var i = 0; i < nbDeDisques; i++) {
+        
+        console.log("disque " + i + ":")
+        drawCercle();
+        currentItem++;
+        cercleSize++;
+        r+=100 // distance des disques
+    }
+    
+}
 
+function drawCercle() {
+
+    // faire un disque
     for(var i = 0; i < items; i++) {
     
+        console.log("current item: " + currentItem)
+        console.log("taille du cercle: " + cercleSize)
+
+        // calcul de la position
         var x = centerX + r * Math.cos(2 * Math.PI * i / items);
         var y = centerY + r * Math.sin(2 * Math.PI * i / items);   
         
-        drawDot(x, y, cercleSize);
+        // repart au début de la palette
+        if (posPalette >= maxLine) 
+        {
+            posPalette = 0;
+        }
 
-        if (currentItem <= 21) {
-            currentItem ++
+        // dessin du point
+        drawDot(x, y, cercleSize, palette[posPalette]);
+        posPalette++;
+
+        // changement de taille du point
+        if (currentItem < maxLine-1) {
+            currentItem++
             cercleSize+= 0.125
         }
         else {
@@ -69,11 +108,9 @@ function createAllCercles() {
 }
 
 
-
-
-function drawDot(x, y, cercleSize) {
+function drawDot(x, y, cercleSize, couleur) {
     ctx.beginPath();
-        ctx.fillStyle="#FF4422"
-        ctx.arc(x, y, cercleSize, 0, 2 * Math.PI);
-        ctx.fill();
+    ctx.fillStyle = couleur
+    ctx.arc(x, y, cercleSize, 0, 2 * Math.PI);
+    ctx.fill();
 }
