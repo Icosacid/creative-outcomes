a1a74a881d5a3114dc830c9b32f9f2f1309abb45
laurabastiani <118973196+laurabastiani@users.noreply.github.com>
2022-11-25 12:30:30 +0100

    finish 3 triangles with good size

--- script.js
+++ script.js
@@ -11,73 +11,46 @@ ctx.strokeStyle = "white"
 ctx.fillStyle = "#F500C2"
 ctx.linewidth = 1
 
-// coté gauche du carré = KENZA
+/
 
 // coté droit du carré = LAURA
 
-// triangle 1
+// triangle haut
 ctx.beginPath();
-    ctx.moveTo(345, 400);
-    ctx.lineTo(345, 0);
-    ctx.lineTo(440, 120);
+    ctx.moveTo(350, 350);
+    ctx.lineTo(350, 0);
+    ctx.lineTo(430, 80);
     ctx.fill();
     ctx.closePath;
 
 
-// triagle 2 (haut)
+// triangle bas
 ctx.beginPath();
-ctx.moveTo (345, 400);
-ctx.lineTo (690, 400);
-ctx.lineTo(600, 310);
- // Triangle plein
- ctx.beginPath();
- ctx.moveTo(307, 307);
- ctx.lineTo(307, 0);
- ctx.lineTo(250, 80);
- ctx.fill();
- ctx.closePath();
-
- // Triangle plein
- ctx.beginPath();
- ctx.moveTo(307, 307);
- ctx.lineTo(0, 307);
- ctx.lineTo(80, 250);
- ctx.fill();
- ctx.closePath();
-
- // Triangle plein
- ctx.beginPath();
- ctx.moveTo(307, 307);
- ctx.lineTo(0, 307);
- ctx.lineTo(80, 370);
- ctx.fill();
- ctx.closePath();
-
-// Triangle plein
-ctx.beginPath();
-ctx.moveTo(307, 307);
-ctx.lineTo(307, 614);
-ctx.lineTo(250, 530);
-ctx.fill();
-ctx.closePath;
-
-// triangle 2 (bas)
-ctx.beginPath();
-ctx.moveTo (345, 400);
-ctx.lineTo (690, 400);
-ctx.lineTo(600, 490);
-ctx.fill();
-ctx.closePath;
-
-// triangle 3
-ctx.beginPath();
-    ctx.moveTo(345, 400);
-    ctx.lineTo(345, 795);
-    ctx.lineTo(440, 680);
+    ctx.moveTo(350, 350);
+    ctx.lineTo(350, 700);
+    ctx.lineTo(430, 620);
     ctx.fill();
     ctx.closePath;
 
 
+// triangle du milieu (bas)
+ctx.beginPath();
+    ctx.moveTo(350, 350);
+    ctx.lineTo(700, 350);
+    ctx.lineTo(620, 430);
+    ctx.fill();
+    ctx.closePath;
+
+    // triangle du milieu (haut)
+
+    ctx.beginPath();
+    ctx.moveTo(350, 350);
+    ctx.lineTo(700, 350);
+    ctx.lineTo(620, 270);
+    ctx.fill();
+    ctx.closePath;
+
+/*
     //petals triangle 1
     ctx.beginPath();
     ctx.moveTo(345, 400);
@@ -93,3 +66,4 @@ ctx.beginPath();
     ctx.lineTo(600, 310);
     ctx.stroke();
     ctx.closePath;
+    */
