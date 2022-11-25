42f775580f8069406f6c10cfcb48e3cdcc65bc6c
laurabastiani <118973196+laurabastiani@users.noreply.github.com>
2022-11-25 11:36:18 +0100

    created first 3 triangles on my side (right)

--- script.js
+++ script.js
@@ -11,3 +11,35 @@ ctx.strokeStyle = "#00F51C"
 ctx.fillStyle = "#F500C2"
 ctx.linewidth = 1
 
+// triangle 1
+ctx.beginPath();
+    ctx.moveTo(345, 400);
+    ctx.lineTo(345, 0);
+    ctx.lineTo(440, 120);
+    ctx.fill();
+    ctx.closePath;
+
+
+// triagle 2 (haut)
+ctx.beginPath();
+ctx.moveTo (345, 400);
+ctx.lineTo (690, 400);
+ctx.lineTo(570, 310);
+ctx.fill();
+ctx.closePath;
+
+// triangle 2 (bas)
+ctx.beginPath();
+ctx.moveTo (345, 400);
+ctx.lineTo (690, 400);
+ctx.lineTo(570, 490);
+ctx.fill();
+ctx.closePath;
+
+// triangle 3
+ctx.beginPath();
+    ctx.moveTo(345, 400);
+    ctx.lineTo(345, 800);
+    ctx.lineTo(440, 680);
+    ctx.fill();
+    ctx.closePath;
